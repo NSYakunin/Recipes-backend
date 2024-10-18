696f076cd5a531dd0b3b959ed6ee893298079bc7
Nickolay <druumm36@gmail.com>
2024-10-18 16:18:02 +0300

    Финалочка(вроде)

--- RecipesEFCore3/RecipeEndpoints.cs
+++ RecipesEFCore3/RecipeEndpoints.cs
@@ -1,4 +1,6 @@
-﻿using RecipesEFCore.DataAccess.SQLServer;
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniValidation;
+using RecipesEFCore.DataAccess.SQLServer;
 using RecipesEFCore3.DTOs;
 using RecipesEFCore3.Models;
 using RecipesEFCore3.Services;
@@ -11,13 +13,19 @@ namespace RecipesEFCore3.Endpoints
         public static void MapRecipeEndpoints(this WebApplication app)
         {
             app.MapPost("/recipes", CreateRecipe);
+
             //app.MapGet("/recipes/{id}", GetRecipeById);
             //app.MapPut("/recipes/{id}", UpdateRecipe);
             //app.MapDelete("/recipes/{id}", DeleteRecipe);
         }
 
-        private static async Task<IResult> CreateRecipe(IRecipeService recipeService, RecipeDto recipeDto)
+        private static async Task<IResult> CreateRecipe(IRecipeService recipeService, [FromBody] RecipeDto recipeDto)
         {
+            if (!MiniValidator.TryValidate(recipeDto, out var errors))
+            {
+                return Results.BadRequest(errors);
+            }
+
             var recipe = await recipeService.CreateRecipeAsync(recipeDto);
             return Results.Created($"/recipes/{recipe.RecipeID}", recipe);
         }
